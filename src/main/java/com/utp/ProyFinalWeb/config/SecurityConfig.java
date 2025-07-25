5eebc10addbf461e3fb2c46ef0a64239a6f2d6e6
Zairgodd19 <U22234704@utp.edu.pe>
2025-07-24 20:18:47 -0500

    Act 2

--- src/main/java/com/utp/ProyFinalWeb/config/SecurityConfig.java
+++ src/main/java/com/utp/ProyFinalWeb/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("https://frontangular-3sg3.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
